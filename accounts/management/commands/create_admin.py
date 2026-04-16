dba69bdc492ff3e47d101a3a3e0d25debce6227c
Abhishek Ghogare <ghogareabhishek4440@gmail.com>
2026-04-16 19:35:43 +0530

    fix: add username to create_superuser

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -7,8 +7,9 @@ class Command(BaseCommand):
         User = get_user_model()
         email = "admin@teople.com"
         password = "Admin@1234"
+        username = "admin"
         if not User.objects.filter(email=email).exists():
-            User.objects.create_superuser(email=email, password=password)
+            User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(f"Superuser created: {email}")
         else:
             self.stdout.write("Superuser already exists")
